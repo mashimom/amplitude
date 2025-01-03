5cefe6c2889e3ec2a17881101246091b5c887e65
Marco Shimomoto <mashimom@gmail.com>
2025-01-04 00:06:07 +0100

    gradle needs to know what it generates

---
+++ src/test/resources/SpockConfig.groovy
@@ -0,0 +1,5 @@
+spockReports {
+	set 'com.athaydes.spockframework.report.projectName': 'Amplitude - a range library'
+	set 'com.athaydes.spockframework.report.showCodeBlocks': true
+//	set 'com.athaydes.spockframework.report.projectVersion': from git?
+}
